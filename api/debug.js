1b749f4bcbc39a3c9b5d629509ab0b787ca3f9ea
ken <Kebwd2004@gmail.com>
2025-08-18 14:43:00 +0800

    fix

--- api/debug.js
+++ api/debug.js
@@ -1,5 +1,4 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.SUPABASE_URL ? 
@@ -8,7 +7,7 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
